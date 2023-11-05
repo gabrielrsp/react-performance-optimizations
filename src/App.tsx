8ecc1c60c939216cd84ff81bb3fbd9686fe11a16
Gabriel <gabriel.pires@a3data.com.br>
2023-11-05 10:29:12 -0300

    memo implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,42 @@
 import React, { FormEvent, useState } from 'react';
 import { SearchResults } from './components/SearchResults';
 
+/**
+ * 3 PRINCIPAIS FORMAS DE UM COMPONENTE RENDERIZAR:
+ *  
+ * 1 - Mudança de hooks
+ * 2 - Mudança no componente Pai
+ * 3 - Mudança em uma Propriedade
+ */
+
+
+/**
+ * CICLO DE RENDERIZAÇÃO
+ * 
+ * 1 - CRIAR UMA NOVA VERSÃO do componente 
+ * 2 - COMPARAR com a versão anterior
+ * 3 - Caso haja alterações, ATUALIZAR o que alterou
+ */
+
+/**
+ * Quando um estado de um componente atualiza, esse componente e todos os seus filhos
+ * renderizam novamente
+ * 
+ * Um novo ciclo de renderização não significa que todos os elementos estão sendo CRIADOS 
+ * no html de novo
+ * 
+ * O react na verdade está COMPARANDO a arvore de elementos novamente pois ele entendeu que precisa
+ * de uma nova renderização
+ * 
+ * Isso nao quer dizer que eles serão recriados do zero
+ * 
+ * O ciclo de renderização entao envolve COMPARAÇÃO e se necessário, CRIAÇÃO de novos elementos
+ * 
+ * Essa comparação também tem um custo de processamento que pode ser alto ou baixo.
+ * 
+ * 
+ */
+
 function App () {
 
   const [search, setSearch] = useState('')
--- src/components/ProductItem.tsx
+++ src/components/ProductItem.tsx
@@ -1,3 +1,22 @@
+import { memo } from 'react'
+
+/**
+ *  O memo é uma função usada por volta de um componente
+ * Caso nenhuma propriedade do componente tenha sido alterada, 
+ * Evita a 1° instrução do ciclo de renderização: Criação de nova versão do componente
+ */
+
+/*
+
+ o memo faz Shallow compare por padrão -> comparação rasa  ( === )
+ 
+ e não a comparação de objetos do javascript (igualdade referencial)
+ {} === {}  // false
+
+*/
+
+
+
 interface ProductItemProps {
   product: {
     id: number;
@@ -6,7 +25,7 @@ interface ProductItemProps {
   }
 }
 
-export function ProductItem ({ product }: ProductItemProps) {
+function ProductItemComponent ({ product }: ProductItemProps) {
   return (
     <div>
       {product.title} - <strong>{product.price}</strong>
@@ -14,3 +33,38 @@ export function ProductItem ({ product }: ProductItemProps) {
   )
 
 }
+
+/* 
+No memo, quando eu preciso de fazer uma comparação que seja de um objeto 
+Eu preciso passar um segundo parâmetro que é a comparação
+
+No caso eu uso uma função que verifica se o objeto é igual ou nao para entao atualzar o componente
+
+Object.is(prevProps, nextProps)
+
+Object.is(): função que faz uma comparação profunda, (CUSTA PROCESSAMENTO)
+
+prevProps: Props antes da renderização
+nextProps: Props depois da renderização
+
+*/
+
+export const ProductItem = memo(ProductItemComponent, (prevProps, nextProps) => {
+
+  return Object.is(prevProps.product, nextProps.product)
+})
+
+
+/* 
+Quando usar o memo
+
+1 - Pure components: Componentes que nao sofrem side effects, dados determinados parametros,
+ela sempre retornara o resultado exclusivamente com base nesses parametros.
+
+2 - Componente que renderizam com muita frequencia
+
+3 - Re-renders com mesmas props
+
+4 - Componente medio pra grande
+
+*/
