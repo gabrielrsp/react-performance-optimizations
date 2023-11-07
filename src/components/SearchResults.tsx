d9e6208c1250fc295111a9b7bdc6d4975632db39
Gabriel <gabriel.pires@a3data.com.br>
2023-11-06 22:26:25 -0300

    implements useMemo

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,5 +1,5 @@
 import { ProductItem } from "./ProductItem";
-
+import { useMemo } from 'react'
 interface SearchResultsProps {
   results: Array<{
     id: number;
@@ -9,8 +9,27 @@ interface SearchResultsProps {
 }
 
 export function SearchResults ({ results }: SearchResultsProps) {
+
+  /* 
+  useMemo 
+  - to memoize a value and avoid unecessary HEAVY calculations
+
+  - Ou entao para manter a igualdade referencial (mesma posição na memoria) evitando que a informação seja recriada do zero, 
+    evitando a verificação/execução do algoritimo de reconciliação 
+    e entao a passando a informação para um componente filho.
+
+  */
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0)
+  }, [results])
+
+
+
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
           <ProductItem product={product} />
